079d72b44af509d58b323e42d44eaa8063be82f0
Daniel Bechler <daniel.bechler@hypoport.de>
2016-03-18 21:50:48 +0100

    Adds a bunch of improvements (Support for legacy Angular, etc.)
    - Support for Angular < 1.3
    - Directive allows configuration of itemHeight
    - Bugfix for cases in which an item gets selected during a digest cycle

--- src/virtual-select-angular.js
+++ src/virtual-select-angular.js
@@ -3,6 +3,9 @@ import VirtualSelect from 'SQiShER/virtual-select';
 function VirtualSelectDirective() {
   function controllerFn() {
     this.formatSearchInput = (item) => {
+      if (!this.optionsProvider) {
+        return '';
+      }
       if (item) {
         return this.optionsProvider.displayText(item);
       }
@@ -14,18 +17,45 @@ function VirtualSelectDirective() {
   }
 
   function linkFn(scope, elem, attrs, [uiVirtualSelectController, ngModelController] , $transclude) {
+    // Since Angular <= 1.3 doesn't support bindToController, we have to handle the binding manually
+    if (angular.version && angular.version.major === 1 && angular.version.minor <= 2) {
+      const bindToController = [
+        'optionsProvider',
+        'onSelectCallback',
+        'onCloseCallback',
+        'onLoadedCallback',
+        'itemHeight',
+      ];
+      // http://blog.thoughtram.io/angularjs/2015/01/02/exploring-angular-1.3-bindToController.html
+      bindToController.forEach(property => {
+        uiVirtualSelectController[property] = scope[property];
+        scope.$watch(property, newValue => {
+          this[property] = newValue;
+        }).bind(uiVirtualSelectController);
+      });
+    }
+
     const customLoadingIndicator = $transclude().siblings('nvs-loading-indicator').eq(0);
-    const virtualselect = new VirtualSelect(elem, {
+    const options = {
       customLoadingIndicator,
       dataProvider: uiVirtualSelectController.optionsProvider,
       onSelect(item) {
-        ngModelController.$setViewValue(item);
-        uiVirtualSelectController.onSelectCallback(item);
+        // we defer this execution in case an item gets selected during a digest cycle
+        scope.$evalAsync(() => {
+          ngModelController.$setViewValue(item);
+          uiVirtualSelectController.onSelectCallback({
+            value: item,
+          });
+        });
       },
       onLoaded() {
         uiVirtualSelectController.onLoadedCallback();
       },
-    });
+    };
+    if (uiVirtualSelectController.itemHeight > 0) {
+      options.itemHeight = parseInt(uiVirtualSelectController.itemHeight, 10);
+    }
+    const virtualselect = new VirtualSelect(elem, options);
 
     scope.$on('ui-virtual-select:focus', () => {
       virtualselect.focus();
@@ -50,6 +80,7 @@ function VirtualSelectDirective() {
     bindToController: true,
     scope: {
       optionsProvider: '=?uiOptionsProvider',
+      itemHeight: '@uiItemHeight',
       onSelectCallback: '&uiOnSelect',
       onCloseCallback: '&uiOnClose',
       onLoadedCallback: '&uiOnLoaded',
